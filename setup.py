8349a4f10b508cbdf92f1f50c0c1d18771dabb0f
shubhammodi <modi.shubham12@gmail.com>
2022-05-31 13:49:17 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='wafer project to implement mlops using mlflow and dvc',
+    author='shubham',
+    license='MIT',
+)
